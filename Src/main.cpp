66f62ba21737f617a29add42bf63194dce1f0be2
gunho <parkgh3737@naver.com>
2019-08-03 21:44:12 +0900

    upload

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,19 +4,16 @@
 
 using namespace std;
 
-int main()
+int func1_encoding()
 {
 	ifstream enFile("encoded.txt");
 	ofstream writeFile("result.txt");
 	string buffer;
 
-	// test1
 	if (enFile.is_open()) 
 	{
-		//enFile >> buffer;
-
 		while (enFile.peek() != EOF) {
-			// std::getline은 입력 스트림에서 string으로 한 줄을 읽습니다.
+			// std::getline은 입력 스트림에서 string으로 한 줄을 읽는다.
 			getline(enFile, buffer);
 			cout << buffer << endl;
 			writeFile << buffer << endl;
@@ -26,7 +23,12 @@ int main()
 		cout << "파일을 찾을 수 없습니다!" << endl;
 	}
 
-	
+	return 0;
+}
 
+int main()
+{
+	func1_encoding();
+	
 	return 0;
 }
